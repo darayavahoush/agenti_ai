0fcd2407609c5390a163789d3a1f5ad78a7109c9
lavanya2kowmar <lavanya2.kowmar@gmail.com>
2026-06-18 01:01:23 +0530

    Disable image route for deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,7 @@ from app.database import Base, engine
 # Routes
 from app.routes.patient import router as patient_router
 from app.routes.speech import router as speech_router
-from app.routes.image import router as image_router
+# from app.routes.image import router as image_router
 
 # -----------------------------------
 # APP
@@ -37,7 +37,7 @@ Base.metadata.create_all(bind=engine)
 # -----------------------------------
 app.include_router(patient_router)
 app.include_router(speech_router)
-app.include_router(image_router)
+# app.include_router(image_router)
 
 # -----------------------------------
 # ROOT
