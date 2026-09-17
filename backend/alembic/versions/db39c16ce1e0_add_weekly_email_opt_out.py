8627005d47528423f10e7ed147c24aadc76623cc
Ananya Sridhar <ananyasridhar108@gmail.com>
2026-09-17 17:10:35 +0530

    Fix placeholder NEWID revision ID, rename to db39c16ce1e0

--- backend/alembic/versions/db39c16ce1e0_add_weekly_email_opt_out.py
+++ backend/alembic/versions/db39c16ce1e0_add_weekly_email_opt_out.py
@@ -18,7 +18,7 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision: str = 'NEWID'
+revision: str = 'db39c16ce1e0'
 down_revision: Union[str, Sequence[str], None] = '886d2530f785'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
